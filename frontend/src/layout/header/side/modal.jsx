061301fa64461580ffbd7da2e54f3f7eeb40c2dd
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2024-12-27 21:36:52 +0700

    Feature: render header side p2

--- frontend/src/layout/header/side/modal.jsx
+++ frontend/src/layout/header/side/modal.jsx
@@ -1,15 +1,28 @@
-import {Avatar, Button, Dropdown, Modal, Typography} from "antd";
+import {Avatar, Button, Typography} from "antd";
 import {LogoutOutlined, QuestionCircleOutlined, SettingOutlined, UserOutlined} from "@ant-design/icons";
 import {NavLink} from "react-router";
+import AccountProfileIcon from "@assets/icons/account.profile.jsx";
+import SignOutIcon from "@assets/icons/signout.jsx";
 
 const {Paragraph, Text} = Typography;
-export default function AccountModal(props){
-	const {modalState, toggleModalState} = props;
+export default function AccountModal(){
+	const items = [
+		{
+			label: 'Account Settings',
+			key: '1',
+			icon: <AccountProfileIcon />
+		},
+		{
+			label: 'Sign Out',
+			key: '2',
+			icon: <SignOutIcon />
+		},
+	];
 
 	return (
-		<>
+		<div className="account-modal">
 			<div className="am-header">
-				<Avatar size="large" icon={<UserOutlined/>} onClick={() => toggleModalState(!modalState)}/>
+				<Avatar size="large" icon={<UserOutlined/>} />
 				<div className="information">
 					<h3>Manh Hung Hoang</h3>
 					<Paragraph>
@@ -26,6 +39,6 @@ export default function AccountModal(props){
 			<div className="am-footer">
 				<Button danger icon={<LogoutOutlined/>}>Sign Out</Button>
 			</div>
-		</>
+		</div>
 	)
-}
+};
--- frontend/src/layout/header/side/side.jsx
+++ frontend/src/layout/header/side/side.jsx
@@ -2,18 +2,34 @@ import {Avatar, Dropdown} from "antd";
 import {QuestionCircleOutlined, SettingOutlined, UserOutlined} from "@ant-design/icons";
 import AccountModal from "@layout/header/side/modal.jsx";
 import {NavLink} from "react-router";
+import AccountProfileIcon from "@assets/icons/account.profile.jsx";
+import SignOutIcon from "@assets/icons/signout.jsx";
 
 export default function SuperHeaderSide(){
+	const items = [
+		{
+			label: 'Account Settings',
+			key: '1',
+			icon: <AccountProfileIcon style={{fontSize: "14px"}}/>
+		},
+		{
+			label: 'Sign Out',
+			key: '2',
+			icon: <SignOutIcon style={{fontSize: "14px"}}/>
+		},
+	];
 
 	return (
 		<div className="super-header-side">
 			<NavLink to={"helpdesk"} className="shs-item"><QuestionCircleOutlined style={{fontSize: "16px"}}/></NavLink>
 			<SettingOutlined style={{fontSize: "16px"}} className="shs-item"/>
 			<Dropdown
-				dropdownRender={() => (<AccountModal />)}
 				trigger={['click']}
 				placement="bottomRight"
 				className="account-dropdown shs-item"
+				menu={{
+					items
+				}}
 			>
 				<Avatar style={{ backgroundColor: '#1F509A' }} icon={<UserOutlined/>} size={"small"}/>
 			</Dropdown>
