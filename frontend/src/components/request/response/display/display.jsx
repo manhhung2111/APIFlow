33647ec435b2a31da4f590e9c0e9a164151e1b96
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2025-03-12 10:50:40 +0700

    Feature: finish response component

--- frontend/src/components/request/response/display/display.jsx
+++ frontend/src/components/request/response/display/display.jsx
@@ -12,14 +12,44 @@ import EmptyResponse from "@assets/images/empty.response.svg"
 export default function RequestResponse(){
 	let {response, request} = useContext(RequestContext);
 
-	const items = [
-		{label: "Body", key: 1, children: <RequestResponseBody />},
-		{label: "Cookies", key: 2, children: <RequestResponseCookies />},
-		{label: "Headers", key: 3, children: <RequestResponseHeaders />},
-		{label: "Test Results", key: 4, children: <RequestResponseTestResults />},
-	];
+	const generateItems = () => {
+		let testResultsLabel = <div className="response-tab-label">Test Results</div>;
+		if(response && typeof response.test_results === "string"){
+			testResultsLabel = <div className="response-tab-label">Test Results
+				<span className="error-badge"></span>
+			</div>;
+		} else if(response && typeof response.test_results !== "string" && response.test_results?.length > 0){
+			const passedTests = response.test_results.reduce((count, test) => {
+				return test.status === "passed" ? count + 1 : count;
+			}, 0);
 
-	if (!request){
+			if(passedTests === response.test_results.length){
+				testResultsLabel = <div className="response-tab-label">Test Results
+					<span className="passed">{`(${passedTests}/${passedTests})`}</span>
+				</div>;
+			} else {
+				testResultsLabel = <div className="response-tab-label">Test Results
+					<span
+						className="error">{`(${response.test_results.length - passedTests}/${response.test_results.length})`}</span>
+				</div>;
+			}
+		}
+
+		let headersLabel = <div className="response-tab-label">Headers</div>;
+		if(Object.keys(response.headers).length > 0){
+			headersLabel = <div className="response-tab-label">Headers <span
+				className="passed">{`(${Object.keys(response.headers).length})`}</span></div>
+		}
+
+		return [
+			{label: <div className="response-tab-label">Body</div>, key: 1, children: <RequestResponseBody/>},
+			{label: <div className="response-tab-label">Cookies</div>, key: 2, children: <RequestResponseCookies/>},
+			{label: headersLabel, key: 3, children: <RequestResponseHeaders/>},
+			{label: testResultsLabel, key: 4, children: <RequestResponseTestResults/>},
+		]
+	}
+
+	if(!request){
 		return "";
 	}
 
@@ -27,7 +57,7 @@ export default function RequestResponse(){
 		<div className="request-response">
 			<div className="rr-header">
 				<h3>Response</h3>
-				{response && !(typeof response === "string") && <RequestResponseSide />}
+				{response && !(typeof response === "string") && <RequestResponseSide/>}
 			</div>
 			{response && typeof response === "string" && <div className="error-message">
 				{response}
@@ -37,7 +67,8 @@ export default function RequestResponse(){
 				<img className="empty-response-img" src={EmptyResponse} alt="Empty Response"/>
 				<p>Click Send to get a response</p>
 			</div>}
-			{response && !(typeof response === "string") && <Tabs items={items} size={"small"} tabBarGutter={8}/>}
+			{response && !(typeof response === "string") &&
+				<Tabs items={generateItems()} size={"small"} tabBarGutter={8}/>}
 		</div>
 	);
 }
